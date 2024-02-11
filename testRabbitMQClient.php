0d7c35918529c6fee72bee4f3e61486830e6e71e
michaelpeluso <mp272.njit>
2024-02-11 13:04:10 -0500

    establish port forwarding connection

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -16,8 +16,8 @@ else
 
 $request = array();
 $request['type'] = "Login";
-$request['username'] = "steve";
-$request['password'] = "password";
+$request['username'] = "mp272";
+$request['password'] = "mp272";
 $request['message'] = $msg;
 $response = $client->send_request($request);
 //$response = $client->publish($request);
