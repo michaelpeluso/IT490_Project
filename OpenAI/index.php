b88faeb9a4971b8d04819c566066eb4534602983
michaelpeluso <mp272.njit>
2024-03-07 02:37:50 -0500

    add functions to ai calls

--- OpenAI/index.php
+++ OpenAI/index.php
@@ -6,21 +6,39 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+$functions = json_decode(file_get_contents('function_descriptions.json'), true);
+
 
 # gather user input
 if (isset($_POST['Submit'])) {
 	$botpromt = $_POST['botpromt'];
 	$query = $_POST['query'];
 }
-/***/
-$query = "My name is Michael.";
-/***/
 
 # connect to API
 $apiKey = $_ENV["CHATGPT_API_KEY"];
 $client = OpenAI::client($apiKey);
 
-# get query result
+
+# generarte a completion
+$prompt = "Get me the details of the best flight from Amsterdam to New York occuring in a week from today.";
+
+$response = $client->chat()->create([
+    'model' => 'gpt-4',
+    'messages' => [
+        ['role' => 'user', 'content' => $prompt],
+    ],
+    'functions' => $functions,
+    'function_call' => 'auto'
+]);
+
+echo $response->usage->totalTokens . " tokens: " . $response->choices[0]->message->functionCall->arguments;
+
+
+/*
+# start a chat
+$query = "My name is Michael.";
+
 $result = $client->chat()->create([
     'model' => 'gpt-4',
     'messages' => [
@@ -30,6 +48,8 @@ $result = $client->chat()->create([
 
 
 echo $result->choices[0]->message->content;
+*/
+
 echo "\n"
 
 
