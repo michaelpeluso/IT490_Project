ec704b39602ecd706ed55b99b879b2cf5d2d8df5
michaelpeluso <mfpel29@gmail.com>
2024-03-19 05:53:29 -0400

    quick push

---
+++ service_pages/fetch_service_data.php
@@ -0,0 +1,40 @@
+<?php
+
+// dependecies
+require_once('../path.inc');
+require_once('../get_host_info.inc');
+require_once('../rabbitMQLib.inc');
+
+// get local service data
+session_start();
+$auth_key = $_SESSION['key'];
+
+$service_id = isset($_GET['service_id']) ? $_GET['service_id'] : null;
+
+// pack data for request
+$request = array(
+    'type' => "post_review",
+    'auth_key' => $auth_key,
+);
+
+// connect to rabbitMQ
+$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+$response = $client->send_request($request);
+
+// log response
+echo "Server response: \n";
+print_r($response);
+
+// validate response
+if ($response['status'] !== "ok") {
+	die("Could not connect to server.");
+}
+
+// parse as json
+$json_response = json_encode($response);
+header('Content-Type: application/json');
+
+// return data
+echo $json_data
+
+?>
